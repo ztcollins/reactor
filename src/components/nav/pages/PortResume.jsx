e9095863294f8c01fe96329b0da8459cdccf77f8
Zachary Collins <ztcollins02@gmail.com>
2023-11-19 19:08:33 -0600

    new pdf tech

--- src/components/nav/pages/PortResume.jsx
+++ src/components/nav/pages/PortResume.jsx
@@ -6,8 +6,8 @@ export default function PortResume(props) {
         <h1>Resume</h1>
         <br/>
         <Container fluid={true}>
-            <p>View my resume <a href="../../../src/assets/zach_resume_2023_upload.pdf">here</a>.</p>
-            <iframe src="../../../src/assets/zach_resume_2023_upload.pdf" width="100%" height="1000px" />
+            <p>View my resume <a href="../../../src/assets/zach_resume_2023_upload.pdf" target="_blank">here</a>.</p>
+            <iframe src={"../../../src/assets/zach_resume_2023_upload.pdf"} width="100%" height="1000px" />
         </Container>
         
     </div>
