ca14bad589bebffd624b684cdf033a5def8201b6
Zachary Collins <ztcollins02@gmail.com>
2023-11-19 21:56:29 -0600

    trying this download thingy out

--- src/components/nav/pages/PortResume.jsx
+++ src/components/nav/pages/PortResume.jsx
@@ -6,7 +6,7 @@ export default function PortResume(props) {
         <h1>Resume</h1>
         <br/>
         <Container fluid={true}>
-            <p>View my resume <a href="../../../src/assets/zach_resume_2023_upload.pdf" target="_blank">here</a>.</p>
+            <p>View my resume <a download href="../../../src/assets/zach_resume_2023_upload.pdf" target="_blank">here</a>.</p>
             <iframe src={"../../../src/assets/zach_resume_2023_upload.pdf"} width="100%" height="1000px" />
         </Container>
         
