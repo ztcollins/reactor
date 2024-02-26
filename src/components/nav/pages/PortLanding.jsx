28621ba5d6838d835fc3883d88a7b36b368024bc
Zachary Collins <ztcollins02@gmail.com>
2024-02-25 19:44:15 -0600

    landing fix

--- src/components/nav/pages/PortLanding.jsx
+++ src/components/nav/pages/PortLanding.jsx
@@ -7,7 +7,7 @@ export default function PortLanding(props) {
             <h1 style={{fontWeight: "bold", textTransform: "uppercase"}}>Zachary <a style={{color: "#2471A3"}}>Collins</a></h1>
             <h5 style={{fontWeight: "bold", textTransform: "uppercase"}}>UW-Madison Student | Software Engineer</h5>
             <h7 style={{textTransform: "uppercase", color: "#868EB0", marginTop: 20}}>Madison, WI - <a href="mailto:ztcollins@gmail.com">ztcollins@gmail.com</a></h7>
-            <p style={{marginTop: 50, color: "#868EB0",}}>I am a software developer that loves to always learn, enjoys building projects and solving challenging problems.</p>
+            <p style={{marginTop: 50, color: "#868EB0",}}>I am a software developer that loves to learn new technology and enjoys solving challenging problems.</p>
             <p style={{marginTop: 50, color: "#868EB0",}}>Check out the other pages that have my work or visit these links below.</p>
             <div fluid>
                 <a style={{marginRight: 20}} href="https://github.com/ztcollins" target="_blank">
